37a34e243d01a669687aa801e0a7dbf6e94e9fdd
jpacsai <judit.pacsai@gmail.com>
2018-07-14 23:04:44 +0200

    reset project

--- ReactRedux/dev/js/index.js
+++ ReactRedux/dev/js/index.js
@@ -2,12 +2,6 @@ import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
 import {Provider} from 'react-redux';
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import promise from 'redux-promise';
-import createLogger from 'redux-logger';
-import allReducers from './reducers';
-import App from './components/App';
 
 const logger = createLogger();
 const store = createStore(
