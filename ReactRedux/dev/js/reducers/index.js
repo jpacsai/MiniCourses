264c938022847831c84119d1e5adab47b8625f10
jpacsai <judit.pacsai@gmail.com>
2018-07-15 13:23:58 +0200

    feat: display user details

--- ReactRedux/dev/js/reducers/index.js
+++ ReactRedux/dev/js/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import UserReducer from './reducer-users';
+import ActiveUser from './reducer-active-user';
 
 const allReducers = combineReducers({
-    users: UserReducer
+    users: UserReducer,
+    activeUser: ActiveUser
 });
 
 export default allReducers;
