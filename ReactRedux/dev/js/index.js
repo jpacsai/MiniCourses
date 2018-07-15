6983cce77147c27021abe4405409d142b1b9adbc
jpacsai <judit.pacsai@gmail.com>
2018-07-15 11:16:44 +0200

    fix: variable names

--- ReactRedux/dev/js/index.js
+++ ReactRedux/dev/js/index.js
@@ -1,12 +1,11 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import allReducers from './reducers'
+import allReducers from './reducers';
+import App from './components/App';
 
 const store = createStore(allReducers);
 
-ReactDOM.render(
-    <h1>Hi</h1>,
-    document.getElementById('root')
-);
+ReactDOM.render(<App />,document.getElementById('root'));
--- ReactRedux/dev/js/reducers/index.js
+++ ReactRedux/dev/js/reducers/index.js
@@ -1,6 +1,8 @@
-import { combineReducers } from 'react';
+import { combineReducers } from 'redux';
 import UserReducer from './reducer-users';
 
 const allReducers = combineReducers({
     users: UserReducer
 });
+
+export default allReducers;
